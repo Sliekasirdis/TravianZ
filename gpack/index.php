9f259601af1be852f443d5bf7142fa33797d8a98
Sliekasirdis <sliekasirdis@outlook.com>
2015-10-12 15:23:07 +0300

    Update index.php

--- gpack/index.php
+++ gpack/index.php
@@ -13,7 +13,7 @@
 	<center>
 		<h1>404 - File not found</h1>
 		<img src="../../gpack/travian_default/img/misc/404.gif" title="Not Found" alt="Not Found"><br />
-		<p>We looked 404 times already but can't find anything, Not even an X marking the spot.</p>
-		<p>This system is not complete yet. So the page probably does not exist.</p><br>
+		<p>Klaida 404</p>
+		<p>Šis puslapis neegzistuoja.</p><br>
 	</center>
 </div>
